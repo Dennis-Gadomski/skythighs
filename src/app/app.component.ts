2c68acc89cc6ecf9e844c7495e3f4f2559aa6984
Dennis Gadomski <dennis.gadomski@gmail.com>
2023-11-23 15:54:42 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,5 +14,5 @@ export class AppComponent {
     { title: 'Spam', url: '/folder/spam', icon: 'warning' },
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  constructor() {}
+  constructor() { }
 }
