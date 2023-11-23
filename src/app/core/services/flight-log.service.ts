048c66e6b69650b0f6fa0f2eb959521c8259edbc
Dennis Gadomski <dennis.gadomski@gmail.com>
2023-11-23 19:07:24 +0100

    implement service

---
+++ src/app/core/services/flight-log.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { FlightLog } from '../interfaces/flight-log.interface';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class FlightLogService {
+
+  FLIGHTS_ENDPOINT = 'flights';
+
+  constructor(private http: HttpClient) { }
+
+  getFlightLogs(): Observable<FlightLog[]> {
+    return this.http.get<{ data: FlightLog[] }>(`${environment.API_URL}/${this.FLIGHTS_ENDPOINT}`).pipe(
+      map(response => response.data)
+    );
+  }
+}
