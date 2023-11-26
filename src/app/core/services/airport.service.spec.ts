2bde8bdb33786ffbd74f459651299b94809e062f
Dennis Gadomski <dennis.gadomski@gmail.com>
2023-11-26 17:53:25 +0100

    add test airport service

--- src/app/core/services/airport.service.spec.ts
+++ src/app/core/services/airport.service.spec.ts
@@ -1,16 +1,43 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { AirportService } from './airport.service';
+import { environment } from 'src/environments/environment';
+import { Airport } from '../interfaces/airport.interface';
 
-describe('AirportService', () => {
+fdescribe('AirportService', () => {
   let service: AirportService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [AirportService]
+    });
+
     service = TestBed.inject(AirportService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should return airport data for the given ident', () => {
+    // ARRANGE
+    const TEST_IDENT = 'EHBK';
+    const mockResponse = {
+      data: {
+        id: 1,
+        ident: TEST_IDENT,
+        name: 'Maastricht-Aachen airport',
+      } as Airport
+    };
+    const httpTestingController = TestBed.inject(HttpTestingController);
+    const testIdent = 'TEST_IDENT';
+
+    // ACT
+    service.getAiportDataByIdent(testIdent).subscribe(airport => {
+      expect(airport).toEqual(mockResponse.data);
+    });
+
+    // ASSERT
+    const req = httpTestingController.expectOne(`${environment.API_URL}/airports/${testIdent}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(mockResponse);
   });
 });
